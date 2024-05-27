5e6b2c8e585be514eeec7800e65431a89dae4e5d
JiaYingWu <jenny0225ki@gmail.com>
2024-05-27 19:00:16 +0800

    解決重新整理重複下單問題

--- src/main/java/com/joyfulresort/jia/roomorder/controller/RoomOrderController.java
+++ src/main/java/com/joyfulresort/jia/roomorder/controller/RoomOrderController.java
@@ -3,31 +3,24 @@ package com.joyfulresort.jia.roomorder.controller;
 import java.io.IOException;
 import java.sql.Date;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BeanPropertyBindingResult;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.joyfulresort.he.member.model.MemberService;
 import com.joyfulresort.he.member.model.MemberVO;
@@ -37,7 +30,6 @@ import com.joyfulresort.jia.roomorderitem.model.RoomOrderItem;
 import com.joyfulresort.jia.roomorderitem.model.RoomOrderItemService;
 import com.joyfulresort.jia.roomschedule.model.RoomSchedule;
 import com.joyfulresort.jia.roomschedule.model.RoomScheduleRedisService;
-import com.joyfulresort.yu.room.model.Room;
 import com.joyfulresort.yu.roomtype.model.RoomType;
 import com.joyfulresort.yu.roomtype.model.RoomTypeService;
 
@@ -165,73 +157,76 @@ public class RoomOrderController {
 	}
 	
 	
-
+//  為了解決在重新整理頁面時重複執行控制器操作並導致重複下單的問題，採用 POST-REDIRECT-GET (PRG) 設計模式。防止當用戶重新整理頁面時重複提交表單。
 	@PostMapping("insert")
-	public String insert(HttpServletRequest req, Model model) throws IOException {
-		String memberPhone = req.getParameter("memberPhone");
+	public String insert(HttpServletRequest req, Model model, RedirectAttributes redirectAttributes) throws IOException {
+	    String memberPhone = req.getParameter("memberPhone");
 
-		MemberVO member = memberSvc.findByMemberPhone(memberPhone);
-		Date checkInDate = java.sql.Date.valueOf(req.getParameter("checkInDate"));
-		Date checkOutDate = java.sql.Date.valueOf(req.getParameter("checkOutDate"));
+	    MemberVO member = memberSvc.findByMemberPhone(memberPhone);
+	    Date checkInDate = java.sql.Date.valueOf(req.getParameter("checkInDate"));
+	    Date checkOutDate = java.sql.Date.valueOf(req.getParameter("checkOutDate"));
 
-		RoomOrder roomOrder = new RoomOrder();
-		roomOrder.setCheckInDate(checkInDate);
-		roomOrder.setCheckOutDate(checkOutDate);
-		roomOrder.setMember(member);
+	    RoomOrder roomOrder = new RoomOrder();
+	    roomOrder.setCheckInDate(checkInDate);
+	    roomOrder.setCheckOutDate(checkOutDate);
+	    roomOrder.setMember(member);
 
-		long diffInMillies = checkOutDate.getTime() - checkInDate.getTime();
-		int differenceInDays = (int) (diffInMillies / (1000 * 3600 * 24));
-		Integer paramCount = Integer.valueOf(req.getParameter("paramCount"));
-		Set<RoomOrderItem> roomOrderItems = new LinkedHashSet<>();
-		Integer roomTypeId = null;
-		Integer roomAmount = 0;
-		
-		for (int i = 1; i <= paramCount; i++) {
-		    roomTypeId = Integer.valueOf(req.getParameter("roomType" + i));
-		    roomAmount = Integer.valueOf(req.getParameter("roomAmount" + i));
-		    if (roomAmount > 0 && roomAmount != null) {
-		        for (int amount = 1; amount <= roomAmount; amount++) {
-		            RoomOrderItem roomOrderItem = new RoomOrderItem(); 
-		            roomOrderItem.setRoomOrder(roomOrder);
-		            roomOrderItem.setRoomType(roomTypeSvc.getOneRoomType(roomTypeId));
-		            roomOrderItem.setRoomPrice(roomTypeSvc.getOneRoomType(roomTypeId).getRoomTypePrice());
-		            roomOrderItem.setSpecialREQ(req.getParameter("specialreq").trim());
-		            Set<RoomSchedule> roomSchedules = new LinkedHashSet<>();
-		            int count2 = 0;
-		            while (count2 < differenceInDays) {
-		                RoomSchedule roomSchedule = new RoomSchedule();
-		                roomSchedule.setRoomType(roomTypeSvc.getOneRoomType(roomTypeId));
-		                roomSchedule.setRoomOderItem(roomOrderItem);
-		                long millies = checkInDate.getTime();
-		                Date date = new Date(count2 * 1000 * 3600 * 24 + millies);
-		                roomSchedule.setRoomScheduleDate(date);
-		                roomSchedules.add(roomSchedule);
-		                count2++;
-		                
-		                rsRedisSvc.addOrDeleteRedisRoomSchedule(date, roomTypeId, 1, -1);
-		              
-		            }
-		            roomOrderItem.setRoomSchedules(roomSchedules);
-		            roomOrderItems.add(roomOrderItem);
-		        }
-		    }
-		}
+	    long diffInMillies = checkOutDate.getTime() - checkInDate.getTime();
+	    int differenceInDays = (int) (diffInMillies / (1000 * 3600 * 24));
+	    Integer paramCount = Integer.valueOf(req.getParameter("paramCount"));
+	    Set<RoomOrderItem> roomOrderItems = new LinkedHashSet<>();
+	    Integer roomTypeId = null;
+	    Integer roomAmount = 0;
 
-		roomOrder.setRoomOrderItems(roomOrderItems);
+	    for (int i = 1; i <= paramCount; i++) {
+	        roomTypeId = Integer.valueOf(req.getParameter("roomType" + i));
+	        roomAmount = Integer.valueOf(req.getParameter("roomAmount" + i));
+	        if (roomAmount > 0 && roomAmount != null) {
+	            for (int amount = 1; amount <= roomAmount; amount++) {
+	                RoomOrderItem roomOrderItem = new RoomOrderItem(); 
+	                roomOrderItem.setRoomOrder(roomOrder);
+	                roomOrderItem.setRoomType(roomTypeSvc.getOneRoomType(roomTypeId));
+	                roomOrderItem.setRoomPrice(roomTypeSvc.getOneRoomType(roomTypeId).getRoomTypePrice());
+	                roomOrderItem.setSpecialREQ(req.getParameter("specialreq").trim());
+	                Set<RoomSchedule> roomSchedules = new LinkedHashSet<>();
+	                int count2 = 0;
+	                while (count2 < differenceInDays) {
+	                    RoomSchedule roomSchedule = new RoomSchedule();
+	                    roomSchedule.setRoomType(roomTypeSvc.getOneRoomType(roomTypeId));
+	                    roomSchedule.setRoomOderItem(roomOrderItem);
+	                    long millies = checkInDate.getTime();
+	                    Date date = new Date(count2 * 1000 * 3600 * 24 + millies);
+	                    roomSchedule.setRoomScheduleDate(date);
+	                    roomSchedules.add(roomSchedule);
+	                    count2++;
+	                    
+	                    rsRedisSvc.addOrDeleteRedisRoomSchedule(date, roomTypeId, 1, -1);
+	                  
+	                }
+	                roomOrderItem.setRoomSchedules(roomSchedules);
+	                roomOrderItems.add(roomOrderItem);
+	            }
+	        }
+	    }
 
-		RoomOrder newRoomOrder = roomOrderSvc.addRoomOrder(roomOrder);
-		
-		model.addAttribute("roomOrder", newRoomOrder);
-		model.addAttribute("totalPrice", req.getParameter("totalPrice"));
-		if(req.getParameter("frontendinsert").equals("true")) {
-			return "front-end/roomorder/listOneRoomOrder";
-		}else {
-			
-			return "back-end/roomorder/listOneRoomOrder";
-		}
-		
+	    roomOrder.setRoomOrderItems(roomOrderItems);
+
+	    RoomOrder newRoomOrder = roomOrderSvc.addRoomOrder(roomOrder);
+
+	    String totalPrice = req.getParameter("totalPrice");
+	    String redirectUrl;
+	    if (req.getParameter("frontendinsert").equals("true")) {
+	        redirectUrl = "redirect:/front-end/roomorder/listOneRoomOrder?roomOrderId=" + newRoomOrder.getRoomOrderId() + "&totalPrice=" + totalPrice;
+	    } else {
+	        redirectUrl = "redirect:/back-end/roomorder/listOneRoomOrder?roomOrderId=" + newRoomOrder.getRoomOrderId() + "&totalPrice=" + totalPrice;
+	    }
+	    return redirectUrl;
 	}
 
+	
+	
+	
+	
 	
 	@PostMapping("checkMember")
 	public ResponseEntity<Map<String, Object>> checkMember(@RequestParam("memberPhone") String memberPhone) {
