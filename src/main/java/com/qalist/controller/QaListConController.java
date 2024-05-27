19690b7c4cd8feb51636fa3d88beaa6c635b70eb
Yuhan <jasmine940703@gmail.com>
2024-05-27 18:53:33 +0800

    刪除Websocket/加入Qalist

---
+++ src/main/java/com/qalist/controller/QaListConController.java
@@ -0,0 +1,25 @@
+package com.qalist.controller;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qalist.model.QaList;
+import com.qalist.model.QaListService;
+
+@RestController
+@RequestMapping("/qalist")
+public class QaListConController {
+
+    @Autowired
+    QaListService qaListService;
+
+    @GetMapping("/reply")
+    public Set<QaList> getQaAnswer(String qaId) {
+        return qaListService.getQaAnswer(qaId);
+    }
+
+}
