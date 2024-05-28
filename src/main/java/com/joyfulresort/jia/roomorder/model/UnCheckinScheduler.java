97e22a557b2fd62552d77026a675297daf20190c
JiaYingWu <jenny0225ki@gmail.com>
2024-05-29 00:14:45 +0800

    修正會員訂房停權問題

--- src/main/java/com/joyfulresort/jia/roomorder/model/UnCheckinScheduler.java
+++ src/main/java/com/joyfulresort/jia/roomorder/model/UnCheckinScheduler.java
@@ -1,14 +1,14 @@
 package com.joyfulresort.jia.roomorder.model;
 
 import java.sql.Date;
-import java.util.List;
-import java.util.Set;
 
-import org.hibernate.Hibernate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.joyfulresort.jia.roomorderitem.model.RoomOrderItem;
 import com.joyfulresort.jia.roomorderitem.model.RoomOrderItemService;
@@ -22,23 +22,33 @@ public class UnCheckinScheduler {
 	@Autowired
 	RoomOrderService roomOrderSvc;
 	
-	 @Scheduled(cron = "0 0 18 * * *") // 每天 18:00 
+	 @Scheduled(cron = "0 0 18 * * *") // 每天 18:00
+	 @Transactional
 	    public void cancelUnCheckinOrders() {
 		 	System.out.println("執行排程器");
-//		 	List<RoomOrder> roomOrders =roomOrderSvc.getAll();
-//	        
-//	        for(RoomOrder roomOrder:roomOrders) {
-//	        	Set<RoomOrderItem> roomOrderItems = roomOrder.getRoomOrderItems();
-//	        	for(RoomOrderItem roomOrderItem:roomOrderItems) {
-//	        		Date date = roomOrder.getCheckInDate();
-//		        	Byte roomOrderItemState = roomOrderItem.getRoomOrderItemState();
-//		        	if(date == new Date(System.currentTimeMillis()) && roomOrderItemState==0) {
-//		        		Hibernate.initialize(roomOrderItem.getRoomOrder().getRoomOrderItems());
-//		                roomOrderItemSvc.cancelRoomOrderItem(roomOrderItem.getRoomOrderItemId());
-//		        	}
-//	        	}
-//	        	
-//	        	
-//	        }
+		 	
+		 	List<RoomOrder> roomOrders =roomOrderSvc.getAll();
+		 	System.out.println(roomOrders);
+	        for(RoomOrder roomOrder:roomOrders) {
+	        	System.out.println(roomOrderItemSvc.getByRoomOrder(roomOrder));
+	        	for(RoomOrderItem roomOrderItem:roomOrderItemSvc.getByRoomOrder(roomOrder)) {
+	        		System.out.println(roomOrderItem);
+	        		String date = roomOrder.getCheckInDate().toString();
+	        		
+		        	Byte roomOrderItemState = roomOrderItem.getRoomOrderItemState();
+		        	String todayDate = new Date(System.currentTimeMillis()).toString();
+		        	System.out.println(date);
+		        	System.out.println(todayDate);
+		        	System.out.println(date.equals(todayDate));
+	        		System.out.println(roomOrderItemState);
+	        		
+		        	if(date.equals(todayDate) && roomOrderItemState==0) {
+		        		System.out.println(roomOrderItem.getRoomOrderItemId());
+		        		roomOrderItemSvc.cancelRoomOrderItem(roomOrderItem.getRoomOrderItemId(),(byte)3);
+		        	}
+	        	}
+	        	
+	        	
+	        }
 	    }
 }
